7d932001f1baee8e74f37b87dbc3393b92cf0619
thamudi <tamim.hamoudi@gmail.com>
2020-11-13 21:07:26 +0200

    Count Day in Quarantine feature added - Now the user has the option to choose either a countdown or count the days in quarantine

--- scripts/app.js
+++ scripts/app.js
@@ -4,13 +4,14 @@
 // ------------------------
 const countDownSectionId = document.getElementById('quarantine-countdown-section');
 const quarantineFormSectionId = document.getElementById('quarantine-form-section');
-const quarantineDaysFormId = document.getElementById('quarantine-form');
+const quarantineDaysFormId = document.getElementById('quarantine-days-form');
+const quarantineCountDaysFormId = document.getElementById('quarantine-count-days-form');
 const resetCountDownButtonId = document.getElementById('countdown-form');
+const countDownContainer = document.getElementById('count-down-container');
+const countContainer = document.getElementById('count-container');
 const countDownEndParId = document.getElementById('target-countdown-end');
 
 const localStorage = window.localStorage; // shorthanded global variable for the local storage window variable
-
-let rendered = false; // Flag used to check the state of the rendered countdown section
 
 // --------------------------------------------------------------------------------------
 // --- helper functions -----------------------------------------------------------------
@@ -27,19 +28,13 @@ function checkLocalStorageState() { // checks if local storage is empty or not
         toggleQuarantineEndMessage('none');
         // Show the countdown Section
         toggleQuarantineCountDownSection('block');
-        // Render the Date paragraph
-        renderDateSection();
-        // Render the countdown script
-        renderCountDownSection();
-        // Switch the the rendered Flag to true for the count down to work
-        rendered = true;
+        // Check local storage state type and render the blocks accordingly 
+        checkLocalStorageStateType();
     } else {
         // Hides the countdown section
         toggleQuarantineCountDownSection('none');
         // Shows the main form section
         toggleQuarantineFormSection('block');
-        // Reset the rendered Flag
-        rendered = false;
     }
 }
 
@@ -49,24 +44,52 @@ function clearLocalStorage(event) { // clears the local storage and then re-chec
     checkLocalStorageState(); // To reset the HTML page state
 }
 
-function setLocalStorageQuarantineValues(dates, countdown) { // Save the desired values into the local storage
+function setLocalStorageQuarantineValues(dates, countdown, countType) { // Save the desired values into the local storage
     localStorage.setItem('endDate', dates.endDateDayMonthText);
     localStorage.setItem('startDate', dates.startDateDayMonthText);
     localStorage.setItem('days', countdown.days);
     localStorage.setItem('hours', countdown.hours);
     localStorage.setItem('minutes', countdown.minutes);
     localStorage.setItem('seconds', countdown.seconds);
-    // console.log(dates);
-    // console.log(countdown);
+    localStorage.setItem('stateType', countType);
+
+}
+
+function checkLocalStorageStateType() { // check if the running state is count or countdown
+    if (localStorage.stateType === 'countdown') {
+        // Hide the Count Block
+        toggleCountContainer('none');
+        // Show the countdown block
+        toggleCountdownContainer('block');
+        // Render the Date paragraph
+        renderCountdownDateSection();
+        // Render the countdown script
+        renderCountDownSection();
+    } else if (localStorage.stateType === 'count') {
+        // Hide the countdown block
+        toggleCountdownContainer('none');
+        // Show the count
+        toggleCountContainer('block');
+        // Render the count Date 
+        renderCountDateSection();
+        // Render the count script
+        renderCountSection();
+    }
 }
 
 // ------------------------
 // --- Form functions -----
 // ------------------------
-function quarantineFormHandler(event) { // Handles user input from the quarantine days form
+function quarantineDaysFormHandler(event) { // Handles user input from the quarantine days form
     event.preventDefault(); // prevents the default behavior of the form
     const days = Math.abs(parseInt(event.target.days.value)); // parse the form input to int and prevent negative values
-    setNewQuarantine(days); // Creates a new quarantine timer properties
+    setNewQuarantine(days, 'countdown'); // Creates a new quarantine timer properties
+    checkLocalStorageState(); // Checks the localStorage state and updates the rendered HTML page
+}
+
+function quarantineCountFormHandler(event) { // Handles user input from the quarantine days form
+    event.preventDefault(); // prevents the default behavior of the form
+    setNewQuarantine(0, 'count'); // Creates a new quarantine timer properties
     checkLocalStorageState(); // Checks the localStorage state and updates the rendered HTML page
 }
 
@@ -86,36 +109,67 @@ function toggleQuarantineEndMessage(display) { // controls the visibility of the
     countDownEndParId.style.display = display;
 }
 
-function renderDateSection() { // Renders the Quarantine date section
+function toggleCountdownContainer(display) { // controls the visibility of the quarantine countdown container
+    countDownContainer.style.display = display;
+}
+
+function toggleCountContainer(display) { // controls the visibility of the quarantine count container
+    countContainer.style.display = display;
+}
+
+function renderCountdownDateSection() { // Renders the Quarantine date section
 
     const countDownParaId = document.getElementById('countdown-date');
-    const target = document.getElementById('target-countdown-date');
-
     let template = countDownParaId.innerHTML;
     let rendered = Mustache.render(template, localStorage);
-    target.innerHTML = rendered;
+    document.getElementById('target-countdown-date').innerHTML = rendered;
+
+}
+
+function renderCountDateSection() { // Renders the Quarantine date section
+
+    const countDownParaId = document.getElementById('count-date');
+    let template = countDownParaId.innerHTML;
+    const date = new QuarantineDate();
+
+    const day = {
+        'today': date.startDayText,
+        'day': date.startDay,
+        'month': date.startMonthText,
+        'year': date.startYear
+    };
+
+    let rendered = Mustache.render(template, day);
+    document.getElementById('target-count-date').innerHTML = rendered;
 
 }
 
 function renderCountDownSection() { // Renders the Quarantine countdown section
 
     const countDownParaId = document.getElementById('countdown');
-    const target = document.getElementById('target-countdown');
-
     let template = countDownParaId.innerHTML;
     let rendered = Mustache.render(template, localStorage);
-    target.innerHTML = rendered;
+    document.getElementById('target-countdown').innerHTML = rendered;
+}
+
+function renderCountSection() { // Renders the Quarantine countdown section
+
+    const countDownParaId = document.getElementById('count');
+    let template = countDownParaId.innerHTML;
+    let rendered = Mustache.render(template, localStorage);
+    document.getElementById('target-count').innerHTML = rendered;
 }
 
 // ------------------------------
 // --- Quarantine functions -----
 // ------------------------------
-function setNewQuarantine(days) { // Creates a new Quarantine Day and Countdown
+function setNewQuarantine(days, type) { // Creates a new Quarantine Day and Countdown
 
     const date = new QuarantineDate(days); // Create a new Quarantine Date
     date.calculateEndDate(); // Calculate the end date for the Quarantine
     const countdown = new CountDown(days); // Create a new countdown
-    setLocalStorageQuarantineValues(date, countdown); // save the created objects properties into the local storage
+    countdown.checkCountUpDayStatus();
+    setLocalStorageQuarantineValues(date, countdown, type); // save the created objects properties into the local storage
 
 }
 
@@ -123,7 +177,6 @@ function setNewQuarantine(days) { // Creates a new Quarantine Day and Countdown
 // --- Countdown functions -----
 // -----------------------------
 function countDown() {
-
     if (localStorage.seconds > 0) {
         localStorage.seconds -= 1;
         renderCountDownSection();
@@ -152,13 +205,52 @@ function countDown() {
 
     }
 }
+function countUp() {
+    const seconds = parseInt(localStorage.seconds);
+    const minutes = parseInt(localStorage.minutes);
+    const hours = parseInt(localStorage.hours);
+    const days = parseInt(localStorage.days);
+    if (localStorage.seconds < 60) {
+        localStorage.seconds = seconds + 1;
+        renderCountSection();
+    } else {
+
+        if (localStorage.minutes < 60) {
+            localStorage.seconds = 0;
+            localStorage.minutes = minutes + 1;
+            renderCountSection();
+        } else {
+            if (localStorage.hours < 60) {
+                localStorage.minutes = 0;
+                localStorage.hours = hours + 1;
+                renderCountSection();
+            } else {
+                if (localStorage.days < 24) {
+                    localStorage.hours = 0;
+                    localStorage.days = days + 1;
+                    renderCountSection();
+                } else {
+                    clearInterval(); // clear the interval function
+                    toggleQuarantineEndMessage('block'); // display the end quarantine message
+                }
+            }
+        }
+
+    }
+}
 
 // ------------------
 // --- Checkers -----
 // ------------------
 function checkCountDownStatus() { // Checks the count down status if its rendered or not
-    if (rendered) {
-        countDown(); // the countdown function, doing its job
+    if (localStorage.length > 0) {
+        if (localStorage.stateType === 'countdown') {
+            countDown(); // the countdown function, doing its job
+        }
+
+        if (localStorage.stateType === 'count') {
+            countUp(); // the count function, doing its job as well, poor guy
+        }
     }
 }
 
@@ -170,7 +262,7 @@ function checkCountDownStatus() { // Checks the count down status if its rendere
 // --- Quarantine Dates Class -----
 // --------------------------------
 class QuarantineDate {
-    
+
     months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     daysArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     date = new Date();
@@ -203,7 +295,7 @@ class QuarantineDate {
         this.endDay = this.startDay + days;
         this.endMonth = this.date.getMonth() + 1;
         this.endYear = this.date.getFullYear();
-        
+
         this.endFormattedDate = this.endYear + '-' + this.endMonth + '-' + this.endDay;
         this.endDate = new Date(this.endFormattedDate);
 
@@ -242,12 +334,22 @@ class CountDown {
         this.minutes = 60 - time.getMinutes();
         this.seconds = 60 - time.getSeconds();
     }
+
+    checkCountUpDayStatus() {
+        if (this.days < 0) {
+            this.days = 0;
+            this.hours = 0;
+            this.minutes = 0;
+            this.seconds = 0;
+        }
+    }
 }
 
 // ----------------------
 // --- event listener ---
 // ----------------------
-quarantineDaysFormId.addEventListener('submit', quarantineFormHandler);
+quarantineDaysFormId.addEventListener('submit', quarantineDaysFormHandler);
+quarantineCountDaysFormId.addEventListener('submit', quarantineCountFormHandler);
 resetCountDownButtonId.addEventListener('submit', clearLocalStorage);
 
 // -------------------
